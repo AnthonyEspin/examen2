0582ca6120d2b6fcb2dca055dccbfe9abfe81bd8
anthony <antho_es@hotmail.com>
2025-06-20 09:24:08 -0500

    web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CircuitoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Si quieres una ruta especial (ejemplo)
+Route::get('/circuitos/mapa', [CircuitoController::class, 'mapa']); // solo si defines ese método en el controlador
+
+// Habilitar rutas resource para Circuito (CRUD completo)
+Route::resource('circuitos', CircuitoController::class);
